9e211c98a8bc6e5d777a62695a32aa4f95cf9a36
suhe <hendarsyahss@gmail.com>
2017-04-05 10:40:43 +0700

    update depkeu

--- extensions/crm_service/models/res_partner.py
+++ extensions/crm_service/models/res_partner.py
@@ -11,3 +11,19 @@ class ResPartner(models.Model):
     audit_assurance_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
                                                    'partner_id', 'service_id', string='Audit &amp; Assurance',
                                                    domain="[('type', '=', 'audit-assurance' )]", )
+    bso_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                                   'partner_id', 'service_id', string='Business Services & Outsourcing',
+                                                   domain="[('type', '=', 'bso' )]", )
+    ras_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                       'partner_id', 'service_id', string='Risk Advisory Services',
+                                       domain="[('type', '=', 'ras' )]", )
+    it_advisory_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                       'partner_id', 'service_id', string='IT Advisory',
+                                       domain="[('type', '=', 'it-advisory' )]", )
+    tax_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                       'partner_id', 'service_id', string='Tax',
+                                       domain="[('type', '=', 'tax' )]", )
+    kjpp_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                               'partner_id', 'service_id', string='KJPP',
+                               domain="[('type', '=', 'kjpp' )]", )
+
