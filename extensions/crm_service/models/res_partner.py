9a74bc24dd386854ce946e1dfe948f305b55c9e1
suhe <hendarsyahss@gmail.com>
2017-04-25 13:41:34 +0700

    update group

--- extensions/crm_service/models/res_partner.py
+++ extensions/crm_service/models/res_partner.py
@@ -8,22 +8,22 @@ class ResPartner(models.Model):
     _description = 'Customer'
     _order = "name asc"
 
-    audit_assurance_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+    audit_assurance_service_ids = fields.Many2many('crm.service', 'res_partner_service_audit_assurance',
                                                    'partner_id', 'service_id', string='Audit &amp; Assurance',
-                                                   domain="[('type', '=', 'audit-assurance' )]", )
-    bso_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                                   domain="[('type', '=', 'audit_assurance' )]")
+    bso_service_ids = fields.Many2many('crm.service', 'res_partner_service_bso',
                                                    'partner_id', 'service_id', string='Business Services & Outsourcing',
-                                                   domain="[('type', '=', 'bso' )]", )
-    ras_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                                   domain="[('type', '=', 'bso' )]" )
+    ras_service_ids = fields.Many2many('crm.service', 'res_partner_service_ras',
                                        'partner_id', 'service_id', string='Risk Advisory Services',
-                                       domain="[('type', '=', 'ras' )]", )
-    it_advisory_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                       domain="[('type', '=', 'ras' )]" )
+    it_advisory_service_ids = fields.Many2many('crm.service', 'res_partner_service_it_advisory',
                                        'partner_id', 'service_id', string='IT Advisory',
-                                       domain="[('type', '=', 'it-advisory' )]", )
-    tax_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                       domain="[('type', '=', 'it-advisory' )]" )
+    tax_service_ids = fields.Many2many('crm.service', 'res_partner_service_tax',
                                        'partner_id', 'service_id', string='Tax',
-                                       domain="[('type', '=', 'tax' )]", )
-    kjpp_service_ids = fields.Many2many('crm.service', 'res_partner_service_rel',
+                                       domain="[('type', '=', 'tax' )]" )
+    kjpp_service_ids = fields.Many2many('crm.service', 'res_partner_service_kjpp',
                                'partner_id', 'service_id', string='KJPP',
-                               domain="[('type', '=', 'kjpp' )]", )
+                               domain="[('type', '=', 'kjpp' )]" )
 
